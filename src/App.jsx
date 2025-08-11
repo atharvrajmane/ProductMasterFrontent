b498e826f7b49e21a82792199262f212178906da
Patilomkar282 <95680955+Patilomkar282@users.noreply.github.com>
2025-08-11 21:58:46 +0530

    Project design Done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductBasicDetailsForm from './components/ProductBasicDetailsForm.jsx';
+
+function App() {
+  
+
+  return (
+    <>
+      <ProductBasicDetailsForm/>
+    </>
+  )
+}
+
+export default App
